d8c933399de2915553a8b52039c8da980a2a9687
Frezill <lephatpeter@gmail.com>
2024-07-22 22:40:05 +0700

    Init client

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        ok ok ok
+      </div>
+    </>
+  )
+}
+
+export default App
