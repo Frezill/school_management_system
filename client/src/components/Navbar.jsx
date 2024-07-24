0fffdf015956b159bcddbf1525ededba0ed54983
Frezill <lephatpeter@gmail.com>
2024-07-24 09:14:06 +0700

    create logout function

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 import logo from '../assets/logo.png'
 import '../styles/Navbar.scss'
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from "react-redux";
+import { setLogout } from "../redux/state";
+import { FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
+
+    const user = useSelector((state) => state.user);
+
+
+    const dispatch = useDispatch()
+    const handleLogout = () => {
+        dispatch(setLogout());
+    }
+
     return (
         <div className="navbar-container">
             <div className="navbar_left">
@@ -17,10 +29,33 @@ const Navbar = () => {
                 <Link to='/introduction'>Introduction</Link>
                 <Link to='/learning'>Learning</Link>
             </div>
+
             <div className="navbar_right">
-                <a><Link to='/login'>Login</Link></a>
-                <a>|</a>
-                <a><Link to='/register'>Register</Link></a>
+                {
+                    user ?
+                        <>
+                            <div className="dropdown-profile">
+                                <div className="profile">
+                                    <FaUserCircle />
+                                </div>
+                                <div >
+                                    <ul className="nav-profile-dropdown">
+                                        <li><Link >Profile</Link></li>
+                                        {user.role === 'student' &&
+                                            <li><Link>Dashboard</Link></li>
+                                        }
+                                        <li onClick={handleLogout}><Link to='/'>Log out</Link></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </>
+                        :
+                        <>
+                            <a><Link to='/login'>Login</Link></a>
+                            <a>|</a>
+                            <a><Link to='/register'>Register</Link></a>
+                        </>
+                }
             </div>
         </div>
     )
