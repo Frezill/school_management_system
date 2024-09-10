e4f6adf2ba06b4c29016e338968ab84e059fb41f
Frezill <lephatpeter@gmail.com>
2024-09-10 21:55:43 +0700

    new update

--- client/src/services/userService.jsx
+++ client/src/services/userService.jsx
@@ -43,7 +43,8 @@ const loginUserService = async (email, password) => {
             toast.error(response.EM)
         }
     } catch (error) {
-        log(error)
+        console.log(error);
+
     }
 }
 
