a8a6825b8a62446dc711f6cbab4bf2414c26aa69
Frezill <lephatpeter@gmail.com>
2024-09-12 09:20:18 +0700

    init

---
+++ server/src/config/config.js
@@ -0,0 +1,36 @@
+require('dotenv/config')
+
+// console.log('Environment variables:');
+// console.log('DB_HOST:', process.env.DB_HOST);
+// console.log('DB_PORT:', process.env.DB_PORT);
+// console.log('DB_DATABASE:', process.env.DB_DATABASE);
+// console.log('DB_USERNAME:', process.env.DB_USERNAME);
+// console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+
+module.exports = {
+  development: {
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3307,
+    define: {
+      freezeTableName: true
+    },
+  }
+};
+
+// module.exports = {
+//   development: {
+//     database: 'school_management',
+//     username: 'root',
+//     password: '123456',
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3307,
+//     define: {
+//       freezeTableName: true
+//     },
+//   }
+// };
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,20 +1,62 @@
 'use strict';
 
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+let customizeConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+  define: {
+    freezeTableName: true
+  }
+};
+
+let sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  customizeConfig
+);
+
+// let customizeConfig = {
+//   host: 'localhost',
+//   port: 3307,
+//   dialect: 'mysql'
+// };
+
+// let sequelize = new Sequelize(
+//   "school_management",
+//   "root",
+//   "123456",
+//   customizeConfig
+// );
+
+(async () => {
+  try {
+    console.log('Current directory:', __dirname);
+    console.log('Environment variables:');
+    console.log('DB_HOST:', process.env.DB_HOST);
+    console.log('DB_PORT:', process.env.DB_PORT);
+    console.log('DB_DATABASE:', process.env.DB_DATABASE);
+    console.log('DB_USERNAME:', process.env.DB_USERNAME);
+    console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+    await sequelize.authenticate()
+    console.log("We have successfully connected to the database!");
+  }
+  catch (e) {
+    console.log("Connection to the database failed!: " + e.message);
+    throw e;
+  }
+})();
+
 
 fs
   .readdirSync(__dirname)
